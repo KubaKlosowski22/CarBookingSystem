d23cd2c121b833e4c28d7a973ab13c7ecc1918c4
Jakub Klosowski <jakub.klosowski@softnauts.com>
2021-03-16 00:11:47 +0100

    Get user and post user endpoints

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
 
 router.get('/', async (req, res) => {
     try{
-        const Users = await User.query();
-        res.send(Users);
-    } catch (err)
-    {
+        const users = await User.query();
+        res.json(users);
+    } catch (err){
         res.status(404);
-        res.send({'err' : err});
+        res.send({message : err});
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const user = await User.query().findById(id);
+        res.json(user);
+    } catch(err){
+        res.status(404);
+        res.send({message : err})
+    }
+})
+
+router.post('/', async (req, res) => {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    
+    try{
+        const user = await User.query().insert({
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        res.status(200);
+        res.json(user);
+    }catch (err){
+        res.status(404);
+        res.send({message : err.stack})
+    }
+})
 module.exports = router;
