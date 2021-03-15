0f976a3f5bad2c88e50a0d15b70c60d49f39a912
Jakub Klosowski <jakub.klosowski@softnauts.com>
2021-03-15 22:01:43 +0100

    DB setup git.dist file

---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+import knex from '../../knexfile';
+import knex from 'knex';
+import knexfile from '../../knexfile';
+
+const env = process.env.NODE_ENV || 'development';
+
+export default knex(knexfile[env]);
---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+import knex from 'knex';
+import { Model } from 'objection';
+import knex from '../config/database';
+
+
+Model.knex(knex);
+
+class User extends Model {
+    static get tableName() {
+        return 'users';
+    }
+}
+
+export default User;
